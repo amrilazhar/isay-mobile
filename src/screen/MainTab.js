9eab86bc571aa8cdd568c02888131771d227e6bc
amrilazhar <amrilazhar@gmail.com>
2021-06-01 19:08:45 +0700

    add emitter in my tab to know if user online

--- src/screen/MainTab.js
+++ src/screen/MainTab.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, View} from 'react-native';
 import NavigationContainer from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -6,6 +6,9 @@ import {color} from '../styles/color';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import socketIOClient from 'socket.io-client';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import jwt_decode from 'jwt-decode';
 
 //Bottom Tab
 import Home from './Home';
@@ -14,9 +17,31 @@ import Notifications from './Notification';
 import Messages from './Messages';
 import Post from './Post';
 
+const ENDPOINT = 'https://isay.gabatch11.my.id/';
 const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
+  useEffect(() => {
+    const io = socketIOClient(ENDPOINT, {
+      transports: ['websocket'],
+      path: '/socket',
+      upgrade: false,
+    });
+    // emit that user online
+
+    AsyncStorage.getItem('accessToken').then(token => {
+      let decodedToken = jwt_decode(token);
+      io.emit('online:' + decodedToken.profile, true);
+    });
+
+    return () => {
+      AsyncStorage.getItem('accessToken').then(token => {
+        let decodedToken = jwt_decode(token);
+        io.emit('online:' + decodedToken.profile, false);
+      });
+    };
+  }, []);
+
   return (
     <Tab.Navigator
       tabBarOptions={{

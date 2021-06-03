fa69eebbaa53ed4bee20e59cad7f580c37045731
Riwan Indra <riwan75@gmail.com>
2021-06-03 15:25:06 +0700

    another

--- src/screen/Profile.js
+++ src/screen/Profile.js
@@ -41,52 +41,7 @@ const Profile = () => {
           keyExtractor={item => item._id}
         />
       </View>
-      {/*======SHOW MODAL===== */}
-      <Modal
-        isVisible={isModalVisible}
-        style={{
-          margin: 0,
-          justifyContent: 'flex-end',
-        }}
-        onBackdropPress={toggleModal}
-        animationIn={'fadeInUp'}
-        animationOut={'fadeOutDown'}
-        useNativeDriver={true}
-        animationInTiming={1000}>
-        <View
-          style={{
-            backgroundColor: color.white,
-            borderColor: 'black',
-            borderColor: 'white',
-            borderTopRightRadius: 20,
-            borderTopLeftRadius: 20,
-          }}>
-          <Text
-            style={{
-              textAlign: 'center',
-              fontSize: 16,
-              marginVertical: 5,
-              fontWeight: 'bold',
-            }}>
-            What topic would you like to see?
-          </Text>
-          <FlatList
-            data={options}
-            renderItem={renderModal}
-            keyExtractor={item => item._id}
-          />
-          <TouchableOpacity
-            onPress={() => {
-              dispatch(getStatusByUserInterestAction());
-              toggleModal();
-            }}>
-            <Text style={{textAlign: 'center', marginVertical: 10}}>
-              By My Interst
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-      {/* ====MODAL==== */}
+      {/*  */}
     </View>
   );
 };
